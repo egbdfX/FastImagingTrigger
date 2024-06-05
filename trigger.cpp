cc4cbcb0920b88cd61d510cd048f012615ae0608
egbdfX <107475738+egbdfX@users.noreply.github.com>
2024-06-05 23:28:36 +0100

    Add files via upload

---
+++ trigger.cpp
@@ -0,0 +1,137 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "fitsio.h"
+#include <vector>
+#include <iostream>
+#include <chrono>
+
+int splittlisi(float* input_data_1, float* input_data_2, float* input_snap, float* result_array, size_t size, size_t unit_size, size_t ima, float maxall);
+
+using namespace std;
+
+float* read_fits_image(const char* filename, long* naxes) {
+    fitsfile *fptr;
+    int status = 0;
+
+    fits_open_file(&fptr, filename, READONLY, &status);
+    if (status) {
+        fits_report_error(stderr, status);
+        return NULL;
+    }
+
+    int naxis;
+    fits_get_img_dim(fptr, &naxis, &status);
+    if (status) {
+        fits_report_error(stderr, status);
+        fits_close_file(fptr, &status);
+        return NULL;
+    }
+    fits_get_img_size(fptr, 2, naxes, &status);
+    if (status) {
+        fits_report_error(stderr, status);
+        fits_close_file(fptr, &status);
+        return NULL;
+    }
+
+    float *image_data = (float *)malloc(naxes[0] * naxes[1] * sizeof(float));
+    if (image_data == NULL) {
+        fprintf(stderr, "Memory allocation failed\n");
+        fits_close_file(fptr, &status);
+        return NULL;
+    }
+
+    fits_read_img(fptr, TFLOAT, 1, naxes[0] * naxes[1], NULL, image_data, NULL, &status);
+    if (status) {
+        fits_report_error(stderr, status);
+        free(image_data);
+        fits_close_file(fptr, &status);
+        return NULL;
+    }
+
+    fits_close_file(fptr, &status);
+    if (status) {
+        fits_report_error(stderr, status);
+        free(image_data);
+        return NULL;
+    }
+
+    return image_data;
+}
+
+int write_fits_image(const char* filename, float *image_data, long* naxes) {
+    fitsfile *fptr;
+    int status = 0;
+
+    fits_create_file(&fptr, filename, &status);
+    if (status) {
+        fits_report_error(stderr, status);
+        return status;
+    }
+
+    long naxis = 2;
+    fits_create_img(fptr, FLOAT_IMG, naxis, naxes, &status);
+    if (status) {
+        fits_report_error(stderr, status);
+        fits_close_file(fptr, &status);
+        return status;
+    }
+
+    fits_write_img(fptr, TFLOAT, 1, naxes[0] * naxes[1], image_data, &status);
+    if (status) {
+        fits_report_error(stderr, status);
+        fits_close_file(fptr, &status);
+        return status;
+    }
+
+    fits_close_file(fptr, &status);
+    if (status) {
+        fits_report_error(stderr, status);
+        return status;
+    }
+
+    return 0;
+}
+
+int main(int argc, char* argv[]) {
+    char input_data_file_1[1000];
+    char input_data_file_2[1000];
+    char input_snap_file[1000];
+    size_t unit_size = 32;
+    size_t ima = 4096;
+    size_t imapow = ima*ima;
+    size_t unit_num = ima/unit_size;
+    long imasize[2];
+    float maxall = 1.0203;
+    
+    sprintf(input_data_file_1, "%s", argv[1]);
+    float *input_data_1 = read_fits_image(input_data_file_1, imasize);
+    if (input_data_1 == NULL) {
+        return 1;
+    }
+    
+    sprintf(input_data_file_2, "%s", argv[2]);
+    float *input_data_2 = read_fits_image(input_data_file_2, imasize);
+    if (input_data_2 == NULL) {
+        return 1;
+    }
+    
+    sprintf(input_snap_file, "%s", argv[3]);
+    float *input_snap = read_fits_image(input_snap_file, imasize);
+    if (input_snap == NULL) {
+        return 1;
+    }
+    
+    float* data_1_array = input_data_1;
+    float* data_2_array = input_data_2;
+    
+    float* result_array = (float*)malloc(unit_num*unit_num*sizeof(float));
+    
+	splittlisi(data_1_array, data_2_array, input_snap, result_array, imapow, unit_size, ima, maxall);
+	
+	long naxes[2] = {long(unit_num), long(unit_num)};
+    int status = write_fits_image("output_tLISI.fits", result_array, naxes);
+    if (status) {
+        fprintf(stderr, "Error writing FITS image\n");
+        return 1;
+    }
+}
---
+++ triggerKernel.cu
@@ -0,0 +1,223 @@
+#include <cuda.h>
+#include <cuda_runtime.h>
+#include <cuda_runtime_api.h>
+#include <iostream>
+#include <cmath>
+
+__device__ size_t floor_device(float x) {
+    return floor(x);
+}
+
+size_t computefloor(float x) {
+    return floor(x);
+}
+
+__device__ float fmod_device(float x, float y) {
+    return fmod(x, y);
+}
+
+size_t computeCeil(float num) {
+    return ceilf(num);
+}
+
+__device__ float ceil_device(float num) {
+    return ceilf(num);
+}
+
+__global__ void setNegativeToZero(float* restored, size_t size) {
+    size_t idx = blockIdx.x * blockDim.x + threadIdx.x;
+    
+    if (idx < size) {
+        restored[idx] = (restored[idx] < 0) ? 0 : restored[idx];
+    }
+}
+
+__global__ void setNonPositiveToC(float* restored, size_t size, float C) {
+    size_t idx = blockIdx.x * blockDim.x + threadIdx.x;
+    
+    if (idx < size) {
+        restored[idx] = (restored[idx] <= 0) ? C : restored[idx];
+    }
+}
+
+__global__ void subtraction_array(float* data_1, float* data_2, float* diff_out, size_t size) {
+    size_t idx = blockIdx.x * blockDim.x + threadIdx.x;
+    if (idx < size) {
+        diff_out[idx] = abs(data_1[idx] - data_2[idx]);
+    }
+}
+
+
+__global__ void tlisi(float* diff_out, float* snap, float* result, size_t size, size_t unit_size, size_t ima, size_t unit_num, float maxall) {
+
+    extern  __shared__  float sharedNumDen[];
+    
+    size_t bid = blockIdx.x;
+    size_t tid = threadIdx.x;
+    
+    size_t i_id = floor_device(bid/unit_num);
+    size_t j_id = fmod_device(bid,unit_num);
+    size_t factor = ceil_device(static_cast<float>(unit_size*unit_size)/1024);
+    
+    size_t I_id;
+    size_t J_id;
+    size_t rows;
+    size_t cols;
+    
+    for (size_t fac = 1; fac <= factor;fac = fac + 1){
+		if (tid+(fac-1)*1024 < unit_size*unit_size){
+			if (fac == 1) {
+				sharedNumDen[tid] = 0; // Sum of diff_out
+				sharedNumDen[tid+1024] = 0; // Max of diff_out
+				sharedNumDen[tid+2048] = 0; // Sum of r
+			}
+			rows = floor_device((tid+(fac-1)*1024)/unit_size);
+			cols = fmod_device((tid+(fac-1)*1024),unit_size);
+			
+			I_id = i_id * unit_size + rows;
+			J_id = j_id * unit_size + cols;
+			
+			sharedNumDen[tid] = sharedNumDen[tid] + diff_out[I_id * ima + J_id];
+			sharedNumDen[tid+1024] = max(sharedNumDen[tid+1024], diff_out[I_id * ima + J_id]);
+			sharedNumDen[tid+2048] = (diff_out[I_id * ima + J_id]/snap[I_id * ima + J_id] < 1) ? sharedNumDen[tid+2048] +  diff_out[I_id * ima + J_id]/snap[I_id * ima + J_id]: sharedNumDen[tid+2048] + 1;
+		} else {
+			if (fac == 1) {
+				sharedNumDen[tid] = 0; // Sum of diff_out
+				sharedNumDen[tid+1024] = 0; // Max of diff_out
+				sharedNumDen[tid+2048] = 0; // Sum of r
+			}
+		}
+	}
+    
+    for (size_t d = blockDim.x/2;d>0;d = d/2){
+		__syncthreads();
+		if (tid<d) {
+			sharedNumDen[tid] += sharedNumDen[tid+d];
+			sharedNumDen[tid+1024] = max(sharedNumDen[tid+1024], sharedNumDen[tid+1024+d]);
+			sharedNumDen[tid+2048] += sharedNumDen[tid+2048+d];
+		}
+	}
+	
+	if (tid==0) {
+		result[bid] = 1-(sharedNumDen[0]/unit_size/unit_size)*sharedNumDen[1024]*(sharedNumDen[2048]/unit_size/unit_size)/maxall/maxall;
+	}
+}
+
+int splittlisi(float* input_data_1, float* input_data_2, float* input_snap, float* result_array, size_t size, size_t unit_size, size_t ima, float maxall) {
+    
+    float* d_data_1;
+    float* d_data_2;
+    float* diff_out;
+    float* snap_data;
+    float* result_data;
+    cudaError_t cudaStatus;
+    float C = 1e-6;
+    
+    size_t unit_num = ima/unit_size;
+    float *diff_in = (float *) malloc(size * sizeof(float));
+    
+    cudaMalloc((void**)&d_data_1, size * sizeof(float));
+    cudaMalloc((void**)&d_data_2,  size * sizeof(float));
+    cudaMalloc((void**)&diff_out,  size * sizeof(float));
+    cudaMalloc((void**)&snap_data,  size * sizeof(float));
+    cudaMalloc((void**)&result_data, unit_num * unit_num * sizeof(float));
+    
+    cudaMemcpy(d_data_1, input_data_1, size * sizeof(float), cudaMemcpyHostToDevice);
+    cudaMemcpy(d_data_2, input_data_2, size * sizeof(float), cudaMemcpyHostToDevice);
+    cudaMemcpy(diff_out, diff_in, size * sizeof(float), cudaMemcpyHostToDevice);
+    cudaMemcpy(snap_data, input_snap, size * sizeof(float), cudaMemcpyHostToDevice);
+    cudaMemcpy(result_data, result_array, unit_num * unit_num * sizeof(float), cudaMemcpyHostToDevice);
+    
+    size_t num_threads = 1024;
+    size_t num_blocks = computeCeil(static_cast<float>(size)/num_threads);
+    setNegativeToZero<<<num_blocks,num_threads>>>(d_data_1, size);
+    cudaStatus = cudaDeviceSynchronize();
+    if (cudaStatus != cudaSuccess) {
+        fprintf(stderr, "Error 1 : %s\n", cudaGetErrorString(cudaStatus));
+	}
+	
+		cudaError_t cudaError;
+		cudaError = cudaGetLastError();
+		if(cudaError != cudaSuccess){
+			printf("ERROR! GPU Kernel error.\n");
+			printf("CUDA error code: %d; string: %s;\n", (int) cudaError, cudaGetErrorString(cudaError));
+		}
+		else {
+			printf("No CUDA error.\n");
+		}
+		
+    setNegativeToZero<<<num_blocks,num_threads>>>(d_data_2, size);
+    cudaStatus = cudaDeviceSynchronize();
+    if (cudaStatus != cudaSuccess) {
+        fprintf(stderr, "Error 2 : %s\n", cudaGetErrorString(cudaStatus));
+	}
+	
+		cudaError = cudaGetLastError();
+		if(cudaError != cudaSuccess){
+			printf("ERROR! GPU Kernel error.\n");
+			printf("CUDA error code: %d; string: %s;\n", (int) cudaError, cudaGetErrorString(cudaError));
+		}
+		else {
+			printf("No CUDA error.\n");
+		}
+	
+	setNonPositiveToC<<<num_blocks,num_threads>>>(snap_data, size, C);
+	cudaStatus = cudaDeviceSynchronize();
+    if (cudaStatus != cudaSuccess) {
+        fprintf(stderr, "Error 3 : %s\n", cudaGetErrorString(cudaStatus));
+	}
+		
+		cudaError = cudaGetLastError();
+		if(cudaError != cudaSuccess){
+			printf("ERROR! GPU Kernel error.\n");
+			printf("CUDA error code: %d; string: %s;\n", (int) cudaError, cudaGetErrorString(cudaError));
+		}
+		else {
+			printf("No CUDA error.\n");
+		}
+    
+    num_threads = 1024;
+    num_blocks = computeCeil(static_cast<float>(size)/num_threads);
+    subtraction_array<<<num_blocks,num_threads>>>(d_data_1, d_data_2, diff_out, size);
+    cudaStatus = cudaDeviceSynchronize();
+    if (cudaStatus != cudaSuccess) {
+        fprintf(stderr, "Error 12 : %s\n", cudaGetErrorString(cudaStatus));
+	}
+		cudaError = cudaGetLastError();
+		if(cudaError != cudaSuccess){
+			printf("ERROR! GPU Kernel error.\n");
+			printf("CUDA error code: %d; string: %s;\n", (int) cudaError, cudaGetErrorString(cudaError));
+		}
+		else {
+			printf("No CUDA error.\n");
+		}
+    
+    num_threads = 1024;
+    num_blocks = unit_num*unit_num;
+    size_t shared_mem_size = 3 * num_threads * sizeof(float);
+    tlisi<<<num_blocks,num_threads,shared_mem_size>>>(diff_out, snap_data, result_data, size, unit_size, ima, unit_num, maxall);
+    cudaStatus = cudaDeviceSynchronize();
+    if (cudaStatus != cudaSuccess) {
+        fprintf(stderr, "Error 13 : %s\n", cudaGetErrorString(cudaStatus));
+	}
+		
+		cudaError = cudaGetLastError();
+		if(cudaError != cudaSuccess){
+			printf("ERROR! GPU Kernel error.\n");
+			printf("CUDA error code: %d; string: %s;\n", (int) cudaError, cudaGetErrorString(cudaError));
+		}
+		else {
+			printf("No CUDA error.\n");
+		}
+
+	cudaMemcpy(result_array, result_data, unit_num * unit_num * sizeof(float), cudaMemcpyDeviceToHost);
+	cudaStatus = cudaDeviceSynchronize();
+    if (cudaStatus != cudaSuccess) {
+        fprintf(stderr, "Error 14 : %s\n", cudaGetErrorString(cudaStatus));
+	}
+
+    cudaFree(d_data_1);
+    cudaFree(d_data_2);
+    cudaFree(result_data);
+    return 0;
+}
